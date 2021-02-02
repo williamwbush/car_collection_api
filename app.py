10034a857a51177d87be9ebe633b56a82b4a6235
William Bush <wbush89@gmail.com>
2021-02-02 02:41:15 -0600

    first commit --WB

---
+++ app.py
@@ -0,0 +1,4 @@
+from dealership_api import app, routes
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ dealership_api/routes.py
@@ -0,0 +1,6 @@
+from dealership_api import app
+from flask import render_template
+
+@app.route('/')
+def home():
+    return render_template('home.html')
